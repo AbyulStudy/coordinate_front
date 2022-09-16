89b96675da9592ab7b37bd43921eeeff216eef3a
Abyul <kinsng1599@gmail.com>
2022-09-16 12:33:37 +0900

    :x:working: width test

--- src/style/main/index.ts
+++ src/style/main/index.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 const SMain = styled.main`
+    background-color: black;
+    margin: 0 auto;
+    width: 100%;
+    max-width: 1180px;
     margin-top: 3.25em;
     height: 20000px;
 `;
