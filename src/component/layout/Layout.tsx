4c1c6460631f483fd8ecc0e9a48117f1d0179add
abyul <kinsng1599@gmail.com>
2022-09-21 09:24:14 +0900

    :x:woriking: 레이아웃 라우트 작업 진행중

--- src/component/layout/Layout.tsx
+++ src/component/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { InputHTMLAttributes, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "styled-components";
 import { SHeader, SNav, SNavMenu, SNavTheme, SThemeToggler, SToggleThumb } from "../../style/header/header";
@@ -9,6 +9,17 @@ const Layout = () => {
     const navigate = useNavigate();
     const { setTheme, theme } = useContext(ThemeContext);
     const [tabNumber, setTabNumber] = useState<number>(1);
+
+    /**
+     * 위도경도 찾기
+     */
+    const [fileName, setFileName] = useState<string>("");
+    const [sendFile, setSendFile] = useState<File>();
+
+    /**
+     *  epsg 개발코드
+     */
+    const [epsgCode, setEpsgCode] = useState<string>("");
 
     const navNavgiate = (path: string) => () => {
         switch (path) {
@@ -23,6 +34,61 @@ const Layout = () => {
                 break;
         }
         navigate(path);
+    };
+    /**
+ * 
+ * [Error] Warning: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`.
+    input
+    div
+    main
+    @http://localhost:3000/static/js/bundle.js:50731:68
+    div
+    @http://localhost:3000/static/js/bundle.js:50731:68
+    Layout@http://localhost:3000/static/js/bundle.js:361:81
+    RenderedRoute@http://localhost:3000/static/js/bundle.js:44617:11
+    Routes@http://localhost:3000/static/js/bundle.js:45065:12
+    Suspense
+    Router
+    Fe@http://localhost:3000/static/js/bundle.js:50676:60
+    App@http://localhost:3000/static/js/bundle.js:38:76
+    Router@http://localhost:3000/static/js/bundle.js:45000:12
+    BrowserRouter@http://localhost:3000/static/js/bundle.js:43218:11
+	printWarning (bundle.js:12310)
+	error (bundle.js:12284)
+	checkControlledValueProps (bundle.js:13804)
+	initWrapperState (bundle.js:13985)
+	setInitialProperties (bundle.js:22133)
+	finalizeInitialChildren (bundle.js:23195)
+	completeWork (bundle.js:34272)
+	completeUnitOfWork (bundle.js:38634)
+	performUnitOfWork (bundle.js:38606)
+	workLoopSync (bundle.js:38508)
+	renderRootSync (bundle.js:38477)
+	performConcurrentWorkOnRoot (bundle.js:37774:93)
+	performConcurrentWorkOnRoot
+	workLoop (bundle.js:49579)
+	flushWork (bundle.js:49553)
+	performWorkUntilDeadline (bundle.js:49837)
+ */
+    const setFileHandler = () => (e: React.ChangeEvent<HTMLInputElement>) => {
+        const fileList: FileList | null = e.target.files;
+        if (fileList) {
+            const fileInfo = fileList[0];
+            const { name, size } = fileInfo;
+            if (size < 2049) {
+                setFileName(name);
+                setSendFile(fileInfo);
+            } else {
+                alert("파일 용량을 확인해주세요 limit:2048");
+            }
+        }
+    };
+
+    const sendFileHandler = () => () => {
+        if (sendFile) {
+        } else {
+            alert("파일을 선택해주세요.");
+        }
     };
 
     return (
@@ -47,7 +113,110 @@ const Layout = () => {
                     </SNavTheme>
                 </SNav>
             </SHeader>
-            <SMain>hi</SMain>
+            <SMain style={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: "0.5em" }}>
+                {/* 위도 경도 찾기 */}
+                {/* <div className="wrap" style={{ display: "flex", flexDirection: "row", marginTop: "1.5em", justifyContent: "space-between" }}>
+                    <div style={{ width: "75%" }}>
+                        <input type="text" value={fileName} style={{ width: "50%" }} />
+                        <input type="file" name="file" id="file" style={{ width: "25%" }} accept="text/plain, text/csv" onChange={setFileHandler()} />
+                    </div>
+                    <div>
+                        <button onClick={sendFileHandler()}>위도경도 찾기</button>
+                    </div>
+                </div>
+                <div style={{ marginTop: "1.5em" }}>
+                    <textarea name="" id="" cols={30} rows={10} readOnly style={{ width: "100%" }}></textarea>
+                </div> */}
+                {/* epsg 개발 코드 */}
+                {/* <div className="wrap" style={{ display: "flex", flexDirection: "row", marginTop: "1.5em", justifyContent: "space-between" }}>
+                    <div style={{ width: "75%" }}>
+                        <input
+                            type="text"
+                            value={epsgCode}
+                            style={{ width: "50%" }}
+                            onChange={e => {
+                                setEpsgCode(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <button onClick={sendFileHandler()}>epsg 코드 검색</button>
+                    </div>
+                </div>
+                <div style={{ width: "100%", display: "flex", flexDirection: "column", padding: "0.5em 1.5em 1.5em 0" }}>
+                    <table style={{ border: "1px solid green" }}>
+                        <thead style={{ textAlign: "start", fontSize: "20px", lineHeight: "36px" }}>
+                            <tr>
+                                <th style={{ border: "1px solid red", textAlign: "start" }}>언어</th>
+                                <th style={{ border: "1px solid red", textAlign: "start" }}>epsg 코드</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>JAVA</td>
+                                <td>epsg.def.....</td>
+                            </tr>
+                            <tr>
+                                <td>JAVA</td>
+                                <td>epsg.def.....</td>
+                            </tr>
+                            <tr>
+                                <td>JAVA</td>
+                                <td>epsg.def.....</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div> */}
+
+                {/* 지도 검색 */}
+                <div className="wrap" style={{ display: "flex", flexDirection: "column", margin: "1em auto 0 auto", gap: "1em" }}>
+                    <div style={{ padding: "1em" }}>
+                        <input type="text" style={{ width: "50%", height: "3em", marginRight: "1em" }} />
+                        <button>검색</button>
+                    </div>
+                    <div style={{ padding: "1em" }}>
+                        <table>
+                            <tbody>
+                                <td>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td>정제/전환 결과데이터</td>
+                                            </tr>
+                                            <tr>
+                                                <td>입력</td>
+                                                <td>경기도 부천시 오정구 성곡로 110-1</td>
+                                            </tr>
+                                            <tr>
+                                                <td>구 주소</td>
+                                                <td>경기도 부천시 오정구 성곡로 110-1</td>
+                                            </tr>
+                                            <tr>
+                                                <td>도로명 주소</td>
+                                                <td>경기도 부천시 오정구 성곡로 110-1</td>
+                                            </tr>
+                                            <tr>
+                                                <td>우편번호</td>
+                                                <td>경기도 부천시 오정구 성곡로 110-1</td>
+                                            </tr>
+                                            <tr>
+                                                <td>좌표</td>
+                                                <td>
+                                                    X :126.1234567890, <br />Y :37.1234567890
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+
+                                <td>
+                                    <div style={{ width: "60vw", height: "60vh", border: "1px solid black" }}></div>
+                                </td>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </SMain>
         </SLayout>
     );
 };
--- src/style/main/index.ts
+++ src/style/main/index.ts
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 const SMain = styled.main`
-    background-color: black;
     margin: 0 auto;
     width: 100%;
+    height: 100vh;
     max-width: 1180px;
     margin-top: 3.25em;
-    height: 20000px;
 `;
 
 export { SMain };
