8179f97d64f2e2b273fb1e3551a2a8238ece3cda
Abyul <kinsng1599@gmail.com>
2022-09-22 16:45:41 +0900

    :x:working: Main Page

---
+++ src/page/main/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SMain, SWrap } from "../../style/main";
+const Home = () => {
+    return (
+        <SMain>
+            <SWrap>hi</SWrap>
+        </SMain>
+    );
+};
+
+export default Home;
