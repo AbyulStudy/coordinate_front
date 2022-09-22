ab5b8bd7362a5105ecd856b710f9e48614cc2cba
Abyul <kinsng1599@gmail.com>
2022-09-22 17:01:45 +0900

    :books:config: Config.ts init

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -2,11 +2,14 @@ export const Config = {
     Server: {
         node: "",
         java: "",
-        local: ""
+        local: "http://localhost:4500"
     },
     Node: {
         epsg: "",
         pro4j: ""
     },
-    kakao: {}
+    Java: { address: "" },
+    kakao: {
+        api: process.env.REACT_APP_KAKAO_KEY
+    }
 };
