74a1e7fe7d9ca5def1bbf6574ee3e2dbbb2a6001
abyul <kinsng1599@gmail.com>
2022-09-27 17:50:26 +0900

    :books:config: axios custom

---
+++ src/config/CustomAxios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { Config } from "./Config";
+
+class AxiosInstance {
+    private static nodeURL = Config.NODE_MODE === "development" ? Config.Local.node : Config.Server.node;
+    private static javaURL = Config.NODE_MODE === "development" ? Config.Local.java : Config.Server.java;
+    private static TIME_OUT = Config.Server.time;
+
+    static createNodeInstance() {
+        return axios.create({
+            baseURL: this.nodeURL,
+            timeout: AxiosInstance.TIME_OUT
+        });
+    }
+
+    static createJavaInstance() {
+        return axios.create({
+            baseURL: this.javaURL,
+            timeout: AxiosInstance.TIME_OUT
+        });
+    }
+}
+
+export default AxiosInstance;
