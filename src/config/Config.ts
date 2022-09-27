b9ddad0f83132cf56fed669e037986737249a99b
abyul <kinsng1599@gmail.com>
2022-09-27 17:50:48 +0900

    :books:config: config update

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,12 +1,18 @@
 export const Config = {
+    NODE_MODE: process.env.NODE_ENV,
     Server: {
         node: "",
         java: "",
-        local: "http://localhost:4500"
+        time: 3000
     },
+    Local: {
+        node: process.env.REACT_APP_NODE_LOCAL,
+        java: process.env.REACT_APP_JAVA_LOCAL
+    },
+
     Node: {
-        epsg: "",
-        pro4j: ""
+        epsg: process.env.REACT_APP_NODE_EPSG,
+        pro4j: process.env.REACT_APP_NODE_PRO4J
     },
     Java: { address: "" },
     kakao: {
